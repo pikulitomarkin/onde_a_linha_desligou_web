146d0cc90bd858069c016419832e42f45428fe72
pikulito <mpadilha932@gmail.com>
2025-10-06 15:19:30 -0300

    Atualizar mapeamento lna_assis para usar assisc1.gpx
    - Alterado mapeamento de 'lna_assis' de 'londrina_assis.gpx' para 'assisc1.gpx'
    - Melhora a precisão dos dados da linha LNA-Assis
    - Arquivo assisc1.gpx já adicionado anteriormente ao repositório

--- kmweb.py
+++ kmweb.py
@@ -32,7 +32,7 @@ class KMAppCore:
             "lon_sdi": "lon_sdi.gpx",
             "lon_mga": "lon_mga.gpx",
             "assis_c2_londrina_norte": "assis2.gpx",
-            "lna_assis": "londrina_assis.gpx",
+            "lna_assis": "assisc1.gpx",
             "ivp_lon": "ivaipora_londrina.gpx",
             "apucarana": "apucarana.gpx",
             "cvo_cvo": "cvo_cvo.gpx",
